4820709c34d1cb9a00a465d441a580f77c46db3b
Saumya <saumyalahera@gmail.com>
2022-04-21 17:28:55 -0700

    Added project files

---
+++ tensor_multiplication_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tensor_multiplication_ios
+//
+//  Created by Saumya Lahera on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
